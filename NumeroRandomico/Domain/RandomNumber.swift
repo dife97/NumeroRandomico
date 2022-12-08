4339a6d3e98ed20e8e9c36bac359ffd4f3fba986
Diego Ferreira <diferodrigues@gmail.com>
2022-12-08 00:15:32 -0300

    Remove Storyboard

--- NumeroRandomico/Domain/RandomNumber.swift
+++ NumeroRandomico/Domain/RandomNumber.swift
@@ -1,8 +1,38 @@
-//
-//  RandomNumber.swift
-//  NumeroRandomico
-//
-//  Created by Diego Ferreira on 07/12/22.
-//
+protocol RandomNumber {
+    
+    var randomNumber: Int { get }
+    
+    var numberOfTries: Int { get }
+    
+    var tryNUmber: Int { get }
+    //TODO: testar se quando o tryNumber foi setado igual ao numberOfTries, a função reset() está sendo invocada
+    
+    func generateRandomNumber(with range: RandomNumberRangeModel) -> Int
+    //TODO: testar se o número armazenado em randomNumber é igual ao número gerado nesta função
+    
+    func checkResult(randomNumber: Int, userNumber: Int) -> RandomNumberResult
+    //TODO: testar se o rightAnswer = true está invocando o reset()
+    //TODO: testar se o greaterOrSmaller realmente é maior ou menor ao número digitado pelo user
+    
+    
+    func reset()
+}
 
-import Foundation
+//TODO: unit test to guarantee the last number is greater than first one
+struct RandomNumberRangeModel {
+    
+    let startNumber: Int
+    let lastNumber: Int
+}
+
+struct RandomNumberResult {
+    
+    let rightAnswer: Bool
+    let greaterOrSmaller: GreaterOrSmaller
+}
+
+enum GreaterOrSmaller {
+    
+    case greater
+    case smaller
+}
--- NumeroRandomico/ViewController.swift
+++ NumeroRandomico/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  NumeroRandomico
-//
-//  Created by Diego Ferreira on 07/12/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
     }
-
-
 }
-
