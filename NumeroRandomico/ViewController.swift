d015412979e1e4f239118ccb61a74f87b31ccbe3
Diego Ferreira <diferodrigues@gmail.com>
2022-12-07 22:11:02 -0300

    Initial Commit

---
+++ NumeroRandomico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumeroRandomico
+//
+//  Created by Diego Ferreira on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
